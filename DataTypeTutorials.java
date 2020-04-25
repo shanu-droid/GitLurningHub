06a08906c02794ab836f6f6820fde680ddb6f32f
shanu-droid <shanug@iitbhilai.ac.in>
2020-04-25 21:46:32 +0530

    new changes in dev

--- DataTypeTutorials.java
+++ DataTypeTutorials.java
@@ -1,15 +1,17 @@
 package Introduction;
 
+import java.text.DecimalFormat;
 public class DataTypeTutorials {
 
 	public static void main(String[] args) {
+		DecimalFormat df = new DecimalFormat("0.00");
 		byte x=2;
 		int first =3;
 		double second = 3.17537831946;
 		float rate = 2.3f;
 		System.out.println(rate);
 		System.out.println(x);
-
+		System.out.println(x+" "+first+" "+df.format(second));
 	}
 
 }
