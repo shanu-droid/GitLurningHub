38c704fbb5d498a56dc492e222365f87ae639037
shanu-droid <shanug@iitbhilai.ac.in>
2020-04-25 17:38:23 +0530

    initial commit

---
+++ DataTypeTutorials.java
@@ -0,0 +1,14 @@
+package Introduction;
+
+public class DataTypeTutorials {
+
+	public static void main(String[] args) {
+		byte x=2;
+		int first =3;
+		double second = 3.17537831946;
+		float rate = 2.3f;
+		System.out.println(rate);
+
+	}
+
+}
